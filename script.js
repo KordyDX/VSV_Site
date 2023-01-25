b0d6b2c9d3fce28ef4cddca6a9652e890d4a0927
Kristián Stefurak <majk123yt@gmail.com>
2023-01-25 11:42:45 +0100

    Update

---
+++ script.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([50.900760785285, 14.62497761563909], 18);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([50.900760785285, 14.62497761563909], {
+    title: "Vzdělávací středisko VDF",
+
+}).addTo(map);
